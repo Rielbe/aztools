8b99ba8ebdfa3f8075e2883f4262c3e603ba98d3
Ismael Chinarro <rielcm97@gmail.com>
2020-07-13 17:29:18 +0200

    Added error checks to the az-cli commands

--- R/blob_utils.R
+++ R/blob_utils.R
@@ -12,12 +12,17 @@
 #' @export
 Download_Batch <- function(az_path = "", local_path = ".", account_name, container_name,
                            sas_token) {
+
   download_query <- paste0("az storage copy -s ",
                            "'https://", account_name, ".blob.core.windows.net/",
                            container_name, "/", az_path, sas_token, "'",
                            " -d ", local_path, " --recursive")
 
-  system(download_query, intern = T)
+  result <- system(download_query, intern = T)
+
+  if (!is.null(attributes(result))) stop("Error al descargar los blobs.")
+
+  result
 }
 
 #' Upload all files from a local directory to an Azure Storage Account's container
@@ -34,12 +39,17 @@ Download_Batch <- function(az_path = "", local_path = ".", account_name, contain
 #' @export
 Upload_Batch <- function(az_path = "", local_path = ".", account_name, container_name,
                          sas_token) {
+
   upload_query <- paste0("az storage copy -d ",
                          "'https://", account_name, ".blob.core.windows.net/",
                          container_name, "/", az_path, sas_token, "'",
                          " -s ", local_path, " --recursive")
 
-  system(upload_query, intern = T)
+  result <- system(upload_query, intern = T)
+
+  if (!is.null(attributes(result))) stop("Error al subir los blobs.")
+
+  result
 }
 
 
@@ -53,11 +63,14 @@ Upload_Batch <- function(az_path = "", local_path = ".", account_name, container
 #' @return List of all blobs inside the directory
 #' @export
 List_Blobs <- function(az_path, account_name, container_name, sas_token) {
+
   list_query <- paste0("az storage blob list -c ", container_name,
                         " --account-name ", account_name, " --prefix ",
                         az_path, " --sas-token '", sas_token, "'")
-
   raw_result <- system(list_query, intern = T)
+
+  if(!is.null(attributes(raw_result))) stop("Error al listar el directorio.")
+
   raw_result <- noquote(noquote(paste(raw_result, collapse = "")))
 
   parsed_list <- rjson::fromJSON(json_str = raw_result, simplify = T)
